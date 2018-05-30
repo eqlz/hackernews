c2d4f9e6f9d2feaacf2f13c9021c0a3ef751f3e6
Emily Qinglu Zhang <emilyqlzhang@gmail.com>
2018-05-29 20:07:44 -0400

    Render a list of posts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    function formatName(user) {
-      return user.firstName + ' ' + user.lastName;
-    }
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-    var user = {
-      firstName: "eli",
-      lastName: "zhang"
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
     };
 
-    var element = <div className="App"><h2>hello {formatName(user)}!</h2></div>;
+    this.onDismiss = this.onDismiss.bind(this);
+  }
 
+  onDismiss(id) {
+    /*
+      function isNotId(item) {
+        return item.objectID !== id;
+      }
+     */
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({list: updatedList});
+  }
+
+  render() {
     return (
-      element
+      <div className="App">
+        {this.state.list.map(item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.url}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+
+              <span>
+                <button onClick={() => this.onDismiss(item.objectID)}
+                        type="button">
+                  Dismiss
+                </button>
+              </span>
+
+            </div>
+          )}
+      </div>
     );
   }
 }
