6f32e81a04f9a17bfb3a864a0d0e55690d81a6ac
Emily Zhang <qinglu.zh@gmail.com>
2018-12-11 21:24:19 -0500

    Put focus on search field via ref

--- src/App.js
+++ src/App.js
@@ -143,19 +143,35 @@ class App extends Component {
   }
 }
 
-const Search = ({value, onChange, onSubmit, children})  => {
-  return (
-    <form onSubmit={onSubmit}>
+class Search extends Component {
+  componentDidMount() {
+    if(this.input) {
+      this.input.focus();
+    }
+  }
+  
+  render() {
+    const {
+      value,
+      onChange,
+      onSubmit,
+      children
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
       <input
         type="text"
         value={value}
         onChange={onChange}
+        ref={(node) => { this.input = node;}}
       />
       <button type="submit">
         {children} 
       </button>
-    </form>
-  )
+      </form>
+    );
+  }
 }
 
 const Table = ({list, onDismiss}) => {
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './App';
+import App, {
+  Search,
+  Button,
+  Table
+} from './App';
 
 describe('App', () => {
-
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render( < App / > , div);
@@ -12,9 +15,59 @@ describe('App', () => {
   });
 
   test('has a valid snapshot', () => {
-    const component = renderer.create(<App />);
+    const component = renderer.create( < App / > );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
-  
+
 });
+
+describe('Search', () => {
+  it('render withou crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render( <Search> Search </Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create( <Search> Search </Search>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Button', () => {
+  it('render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render( <Button> Click me, I 'm a button!</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create( <Button> Search </Button>);
+    let tree = component.toJSON(); 
+    expect(tree).toMatchSnapshot();
+  })
+})
+
+describe('Table', () => {
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+})
